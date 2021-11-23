89c7c9c66f193d764dea92244f4bac69f925421a
adit777 <adit.patange@gmail.com>
2021-11-23 15:23:35 +0530

    Display.java updated

---
+++ src/Display.java
@@ -0,0 +1,14 @@
+
+public class Display {
+public static void main(String[] args) {
+	
+	System.out.println("Hello world from git project!!!");
+	
+	System.out.println("THE FILEREWRITE AFTER ADD IN STAGING AREA");
+	
+	int a=5,b=6;
+	
+	int result = a+b;
+	System.out.println(result);
+}
+}
